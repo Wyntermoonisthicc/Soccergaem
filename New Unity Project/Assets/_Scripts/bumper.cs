ab14b0316286af5ec09a98fb0111cbde2d746300
matthew <mblathwayt@gmail.com>
2019-08-23 11:35:43 +1200

    stuff

---
+++ New Unity Project/Assets/_Scripts/bumper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bumper : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =  new Vector3(Mathf.PingPong(Time.time, 9), 0, -58);
+    }
+}
